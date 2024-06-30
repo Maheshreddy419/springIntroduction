7d013da11138d2fb38e0110872c1ed9dee902301
Maheshreddy419 <mahesh.apple62@gmail.com>
2024-06-30 18:13:02 +0530

    1.2 setting up

---
+++ IntroductionTospringboot/src/main/java/com/codingshuttle/week1/IntroductionTospringboot/IntroductionTospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.week1.IntroductionTospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionTospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionTospringbootApplication.class, args);
+	}
+
+}
---
+++ IntroductionTospringboot/src/test/java/com/codingshuttle/week1/IntroductionTospringboot/IntroductionTospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.week1.IntroductionTospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionTospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
